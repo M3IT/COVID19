e475f097a582e7dd259a49b719d6a77cb5dba6a1
Matt Bolton <gitkraken@m3it.com.au>
2020-07-31 01:48:21 +1000

    Update ds_covid19au_git.R
    New source file name and fixed column headers

--- R/ds_covid19au_git.R
+++ R/ds_covid19au_git.R
@@ -1,16 +1,15 @@
 covid19au_git <- function(level, cache) {
     # Author: Matt Bolton (github.com/M3IT)
     # Date:   2020-06-13
-    
-    # Data aggregated from www.covid19data.com.au, which is sourced from Australian governments at federal, state and territory levels. 
+
+    # Data aggregated from www.covid19data.com.au, which is sourced from Australian governments at federal, state and territory levels.
     # See www.covid19data.com.au/data-notes for details and notes.
 
     # Read data
     repo <- "https://raw.githubusercontent.com/M3IT/COVID-19_Data"
-    url  <- "/master/Data/COVID_Data_Hub.csv"
+    url  <- "/master/Data/COVID19_Data_Hub.csv"
 
     x    <- read.csv(paste0(repo,url), cache = cache)
-    colnames(x) <- gsub("COVID_Data_Hub.", "", colnames(x), fixed = TRUE)
 
     # Minimal additional code as data already formatted as required
     x$date <- as.Date(x$date)
